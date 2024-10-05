62dad26da89f95896bafd428991c9aa6a5767814
sohbanm <99363017+sohbanm@users.noreply.github.com>
2024-10-05 16:26:14 -0400

    stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,34 +3,78 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [selectedFileData, setSelectedFileData] = useState<File | null>(null);
+  const [selectedFileBase, setSelectedFileBase] = useState<File | null>(null);
 
-  const handleFileDrop = (e: React.DragEvent<HTMLDivElement>) => {
+  const handleFileDropBase = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     const files = e.dataTransfer.files;
     if (files && files.length > 0) {
-      setSelectedFile(files[0]);
+      setSelectedFileBase(files[0]);
+      console.log("Dropped file: ", files[0]);
+    }
+  };
+  const handleFileDropData = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+    if (files && files.length > 0) {
+      setSelectedFileData(files[0]);
       console.log("Dropped file: ", files[0]);
     }
   };
 
-  const handleFileSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileSelectBase = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     if (files && files.length > 0) {
-      setSelectedFile(files[0]);
+      setSelectedFileBase(files[0]);
+      console.log("Selected file: ", files[0]);
+    }
+  };
+  const handleFileSelectData = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files && files.length > 0) {
+      setSelectedFileData(files[0]);
       console.log("Selected file: ", files[0]);
     }
   };
 
-  const handleClick = () => {
-    document.getElementById("fileInput")?.click(); // Programmatically click the hidden input
+  const handleClickBase = () => {
+    document.getElementById("fileInputBase")?.click(); // Programmatically click the hidden input
+  };
+  const handleClickData = () => {
+    document.getElementById("fileInputData")?.click(); // Programmatically click the hidden input
   };
 
-  const handleSubmit = async () => {
-    if (!selectedFile) return;
+  const handleSubmitBase = async () => {
+    if (!selectedFileBase) return;
 
     const formData = new FormData();
-    formData.append("file", selectedFile);
+    formData.append("file", selectedFileBase);
+
+  try {
+    const response = await axios.post("http://localhost:8000/upload", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data", // Set the content type for file uploads
+      },
+    });
+
+    if (response.status === 200) {
+      console.log("File uploaded successfully!");
+    } else {
+      console.error("File upload failed.");
+    }
+  } catch (error) {
+    console.error("Error while uploading file:", error);
+  }
+  };
+
+  const handleSubmitData = async () => {
+    if (!selectedFileData) return;
+    if (!selectedFileBase) return;
+
+    const formData = new FormData();
+    formData.append("file", selectedFileData);
+    formData.append("file1", selectedFileBase);
 
   try {
     const response = await axios.post("http://localhost:8000/upload", formData, {
@@ -51,36 +95,71 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">File Drop Zone</h1>
-      
-      <div
-        className="drop-zone border-2 border-dashed border-gray-400 p-4 w-64 h-32 flex justify-center items-center"
-        onClick={handleClick}
-        onDrop={handleFileDrop}
-        onDragOver={(e) => e.preventDefault()}
-      >
-        {selectedFile ? (
-          <p>{selectedFile.name}</p>
-        ) : (
-          <p>Drop a file here or click to upload</p>
-        )}
+      <div className="flex">
+        <div className="block">
+          <div
+            className="drop-zone border-2 border-dashed border-gray-400 p-4 w-64 h-32 flex justify-center items-center"
+            onClick={handleClickData}
+            onDrop={handleFileDropData}
+            onDragOver={(e) => e.preventDefault()}
+          >
+            {selectedFileData ? (
+              <p>{selectedFileData.name}</p>
+            ) : (
+              <p>Drop a file here or click to upload</p>
+            )}
+          </div>
+
+          {/* Hidden file input */}
+          <input
+            id="fileInputData"
+            type="file"
+            style={{ display: "none" }}
+            onChange={handleFileSelectData}
+          />
+        </div>
+
+        <div className="block">
+          <div
+            className="drop-zone border-2 border-dashed border-gray-400 p-4 w-64 h-32 flex justify-center items-center"
+            onClick={handleClickBase}
+            onDrop={handleFileDropBase}
+            onDragOver={(e) => e.preventDefault()}
+          >
+            {selectedFileBase ? (
+              <p>{selectedFileBase.name}</p>
+            ) : (
+              <p>Drop a file here or click to upload</p>
+            )}
+          </div>
+
+          {/* Hidden file input */}
+          <input
+            id="fileInputBase"
+            type="file"
+            style={{ display: "none" }}
+            onChange={handleFileSelectBase}
+          />
+
+          {/* {selectedFileBase && (
+            <button
+              onClick={handleSubmitBase}
+              className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
+            >
+              Upload File
+            </button>
+        )} */}
+        </div>
+        {selectedFileData && selectedFileBase &&(
+            <button
+              onClick={handleSubmitData}
+              className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
+            >
+              Upload File
+            </button>
+          )}
+
       </div>
-
-      {/* Hidden file input */}
-      <input
-        id="fileInput"
-        type="file"
-        style={{ display: "none" }}
-        onChange={handleFileSelect}
-      />
-
-      {selectedFile && (
-        <button
-          onClick={handleSubmit}
-          className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
-        >
-          Upload File
-        </button>
-      )}
     </>
   );
 }
--- server/main.py
+++ server/main.py
@@ -10,25 +10,53 @@ async def root():
     return {"message": "Hello World"}
 
 @app.post("/upload")
-async def upload_file(file: UploadFile = File(...)):
-    if file.content_type != "application/pdf":
+async def upload_file(file1: UploadFile = File(...), file2: UploadFile = File(...) ):
+    if file1.content_type != "application/pdf" and file2.content_type != "application/pdf":
         return {"error": "Only PDF files are allowed"}
 
     # Read the contents of the file
-    contents = await file.read()
+    contents1 = await file1.read()
+    contents2 = await file2.read()
 
     # Convert bytes to a file-like object
-    pdf_file = BytesIO(contents)
+    pdf_file1 = BytesIO(contents1)
+    pdf_file2 = BytesIO(contents2)
 
     # To process the PDF, use a library like PyPDF2
-    pdf_reader = PyPDF2.PdfReader(pdf_file)
+    pdf_reader1 = PyPDF2.PdfReader(pdf_file1)
+    pdf_reader2 = PyPDF2.PdfReader(pdf_file2)
 
     # Extract text from the first page as an example
-    first_page = pdf_reader.pages[0]
-    text = first_page.extract_text()
-    print("worked")
-    print(file.filename)
-    print(text)
+    first_page1 = pdf_reader1.pages[0]
+    text1 = first_page1.extract_text()
+    first_page2 = pdf_reader2.pages[0]
+    text2 = first_page2.extract_text()
+    print(file1.filename)
+    print(text1)
+    print(file2.filename)
+    print(text2)
 
-    return {"filename": file.filename, "content": text}
+    return {"filename1": file1.filename, "filename2": file2.filename}
+    
+# @app.post("/uploadBase")
+# async def upload_file(file: UploadFile = File(...)):
+#     if file.content_type != "application/pdf":
+#         return {"error": "Only PDF files are allowed"}
+
+#     # Read the contents of the file
+#     contents = await file.read()
+
+#     # Convert bytes to a file-like object
+#     pdf_file = BytesIO(contents)
+
+#     # To process the PDF, use a library like PyPDF2
+#     pdf_reader = PyPDF2.PdfReader(pdf_file)
+
+#     # Extract text from the first page as an example
+#     first_page = pdf_reader.pages[0]
+#     text = first_page.extract_text()
+#     print(file.filename)
+#     print(text)
+
+#     return {"filename": file.filename, "content": text}
     
